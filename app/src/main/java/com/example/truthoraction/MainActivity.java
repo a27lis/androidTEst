7767bd174461f33c1c4b9608d9352cdee5a0d9c8
a27lis <anastasiya.liseykina@gmail.com>
2020-09-28 18:29:18 +0400

    hello

---
+++ app/src/main/java/com/example/truthoraction/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.truthoraction;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView top, bottom;
+    Animation topAnim, bottomAnim;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        top = findViewById(R.id.topText);
+        bottom = findViewById(R.id.bottomText);
+
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_anim);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_anim);
+
+        top.setAnimation(topAnim);
+        bottom.setAnimation(bottomAnim);
+
+
+
+    }
+}
